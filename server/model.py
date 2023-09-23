5593a2acf227bea5b08866d70ea5df1be853a7ed
sam kariuki <samuel.kariuki1@student.moringaschool.com>
2023-09-23 11:45:12 +0300

    created pizzas model

--- server/model.py
+++ server/model.py
@@ -1,4 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+
 db = SQLAlchemy()
+
+class Pizzas(db.Model):
+    __tablename__ = "pizza"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique = True, nullable =False)
+    ingredients = db.Column(db.String, nullable= Flase)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+
+
+
 
